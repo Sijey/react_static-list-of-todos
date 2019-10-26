1624706110a82e14c5ab18923a03363d039cf6ec
Sijey <Popov040391@gmail.com>
2019-10-26 17:28:48 +0300

    Updates

--- precache-manifest.b3846172aa550210ebdb30b7ee9fafcf.js
+++ precache-manifest.b3846172aa550210ebdb30b7ee9fafcf.js
@@ -1,19 +1,19 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "9f4c0541055d1cb80dc84d525e26cd92",
+    "revision": "b46d6c526f1db738b2d11591157f3bff",
     "url": "/react_static-list-of-todos/index.html"
   },
   {
-    "revision": "b40e9ca27fda17825a70",
+    "revision": "a77b73543ebd1b9dff67",
     "url": "/react_static-list-of-todos/static/css/main.cb9e985a.chunk.css"
   },
   {
-    "revision": "2315f74ce3c99316133d",
-    "url": "/react_static-list-of-todos/static/js/2.3310f33a.chunk.js"
+    "revision": "15dfd4d495562257ee23",
+    "url": "/react_static-list-of-todos/static/js/2.9360f46c.chunk.js"
   },
   {
-    "revision": "b40e9ca27fda17825a70",
-    "url": "/react_static-list-of-todos/static/js/main.1d2983b2.chunk.js"
+    "revision": "a77b73543ebd1b9dff67",
+    "url": "/react_static-list-of-todos/static/js/main.5e961a69.chunk.js"
   },
   {
     "revision": "ea6f31786c2aed313835",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/react_static-list-of-todos/precache-manifest.1460eb5f6aa52629add7e99c8f3a4e44.js"
+  "/react_static-list-of-todos/precache-manifest.b3846172aa550210ebdb30b7ee9fafcf.js"
 );
 
 self.addEventListener('message', (event) => {
